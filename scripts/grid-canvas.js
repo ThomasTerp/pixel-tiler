5d11790687585fdb6fb3e2e559bcd7d1a304398e
Thomas Terp <thomascterp@gmail.com>
2021-04-09 11:35:23 +0200

    Improved grid

--- scripts/grid-canvas.js
+++ scripts/grid-canvas.js
@@ -41,7 +41,7 @@ export default class GridCanvas extends HTMLObject
 	buildHTML()
 	{
 		return $(`
-			<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+			<svg width="100%" height="100%" viewBox="-1000,-1000 2000,2000" xmlns="http://www.w3.org/2000/svg">
 				<defs>
 					<pattern id="subGrid" width="${this._gridSize}" height="${this._gridSize}" patternUnits="userSpaceOnUse">
 						<path d="M0,${this._gridSize} L0,0 L${this._gridSize},0" fill="none" stroke="grey" stroke-width="1" />
@@ -52,7 +52,9 @@ export default class GridCanvas extends HTMLObject
 					</pattern>
 				</defs>
 
-				<rect width="100%" height="100%" fill="url(#grid)" />
+				<rect x="-100%" y="-100%" width="200%" height="200%" fill="url(#grid)" />
+				<line x1="-100%" y1="0" x2="100%" y2="0" stroke="black" stroke-width="2" />
+				<line x1="0" y1="-100%" x2="0" y2="100%" stroke="black" stroke-width="2" />
 			</svg>
 		`);
 	}
